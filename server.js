acbaa19a5486803b3eba2a584711b9430a9cdf46
DILEGENT1 <di.abaki@gmail.com>
2024-01-01 14:28:05 +0400

    error sample

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require('http');
-
+const todos = [
+    { id: 1, text: 'Learning Rust'},
+    { id: 2, text: 'Learning Python'},
+    { id: 3, text: 'Learning Java'},
+];
 const server = http.createServer((req, res) => {
-    const {headers, url, method } = req;
-    console.log(headers, url, method);
-    res.setHeader('Content-Type','text/html');
-    res.setHeader('Anything','anything');
-    res.write('<h1>Hello world</h1>');
-    res.write('<h2>Hello NodeJs</h2>');
-    res.end();
+    
+    //res.setHeader('Content-Type','application/json');
+    res.writeHead(400, {
+        'Content-Type': 'application/json',
+        'UserAccount':'something'
+    });
+    res.end(JSON.stringify({
+        success: false,
+        error: 'Not found',
+        data: {},
+    }));
 });
 const PORT = 5001;
 server.listen(PORT, () => console.log(`server is running on ${PORT}`));
