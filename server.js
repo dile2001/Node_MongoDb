440bd0218cf8f9a97b0ef9816f52a7630efa1c9c
DILEGENT1 <di.abaki@gmail.com>
2024-01-07 11:36:24 +0400

    use morgan logger

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+//const logger = require('./middleware/logger');
+const morgan = require('morgan');
 //Router 
 const bootcamps = require('./routes/bootcamps');
 dotenv.config({path: './config/config.env'});
 const app = express();
-
-app.use(logger);
+// Middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+//app.use(logger);
 //Mount route
 app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5001;
