1e5dbf7d55498f639f2818faec111c21b02cd2fd
DILEGENT1 <di.abaki@gmail.com>
2024-01-23 06:13:48 +0400

    error handler middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors'); 
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 dotenv.config({path: './config/config.env'});
 
 connectDB();
@@ -21,6 +22,7 @@ if(process.env.NODE_ENV === 'development'){
 //app.use(logger);
 //Mount route
 app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, console.log(`running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
 // Handle unhandled promise rejections 
